0f84ff763c67dbb16fd78a6a0bffba1155c13635
Dan Maljovec <maljovec002@gmail.com>
2018-12-03 12:29:05 -0700

    Working implementation of recording which mandatory critcial point region a point belongs to.

--- MorseComplex.py
+++ MorseComplex.py
@@ -13,16 +13,61 @@ class MorseComplex2D(object):
     def __init__(self, xComponentMatrix, yComponentMatrix, mandatoryMaxima):
         self.X = xComponentMatrix
         self.Y = yComponentMatrix
-        self.mandatoryMaxima = mandatoryMaxima
+        self.nRows, self.nCols = self.X.shape
 
-    def getMandatoryFlowField(self):
-        pass
+        self.maxFlow = self._computeMandatoryFlowField(mandatoryMaxima)
 
-    def getFlowField(self):
-        pass
+    def _computeMandatoryFlowField(self, mandatoryMaxima):
+        maxIndex = self.nRows*self.nCols
 
-    def getMandatoryFlow(self, i, j):
-        pass
+        # Offset the maxFlow indices so as not to collide with any
+        # existing ids, we'll remove this offset at the end
+        self.maxFlow = mandatoryMaxima
+        self.maxFlow += maxIndex
 
-    def getFlow(self, i, j):
+        # Initialize the union-find data structure so we can
+        # short-circuit the algorithm
+        self.uf = UnionFind()
+        for i in np.unique(self.maxFlow):
+            self.uf.MakeSet(i)
+
+        # Initialize the union-find with items where we know the
+        # solution
+        for row in range(self.nRows):
+            for col in range(self.nCols):
+                idx = row*self.nCols+col
+                self.uf.MakeSet(idx)
+                if self.maxFlow[row, col] >= maxIndex:
+                    self.uf.Union(self.maxFlow[row, col], idx)
+
+        # Now compute the rest
+        for row in range(self.nRows):
+            for col in range(self.nCols):
+                idx = row*self.nCols+col
+                touched = set()
+                destination = self.trace(row, col, touched)
+                self.uf.Union(idx, destination)
+                self.maxFlow[row, col] = destination
+
+        self.maxFlow -= maxIndex
+        return self.maxFlow
+
+    def trace(self, row, col, touched):
+        idx = row*self.nCols+col
+        if idx in touched:
+            return idx
+        touched.add(idx)
+        flow = self.uf.Find(idx)
+        if flow >= self.nRows*self.nCols:
+            return flow
+        else:
+            moveX = self.X[row, col]
+            moveY = self.Y[row, col]
+            newRow = row + moveY
+            newCol = col + moveX
+            if newRow == row and newCol == col:
+                return idx
+            return self.trace(newRow, newCol, touched)
+
+    def _computeFlowField(self):
         pass
